bebf879848677d2ddf2b0164f4335053228470f7
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-03-26 12:57:08 +0530

    feat(db.js): add MongoDB client configuration to connect to the database using the MONGODB_URI environment variable feat(package.json): update dependencies to include @auth/mongodb-adapter and mongodb packages feat([...nextauth].js): import MongoDBAdapter and client from db.js to configure MongoDBAdapter for NextAuth adapter option

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import client from "./lib/db";
 
 export default NextAuth({
+  adapter: MongoDBAdapter(client),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
