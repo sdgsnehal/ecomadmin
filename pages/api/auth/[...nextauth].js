65f750f003f6ce8940058f425a1437bec92f0bac
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-03-27 11:19:10 +0530

    feat(package.json): add 'concurrently' package to run 'kill-port 3000' before 'next dev' script to prevent port conflicts feat(package.json): add 'kill-port' package to be able to kill processes running on a specific port refactor([...nextauth].js): update import path for 'client' to use absolute path alias '@/lib/db' for better clarity refactor([...nextauth].js): move 'adapter' configuration to the end of the NextAuth configuration object for consistency

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,14 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import client from "./lib/db";
+import client from "@/lib/db";
 
 export default NextAuth({
-  adapter: MongoDBAdapter(client),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+  adapter: MongoDBAdapter(client),
 });
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 export default function Home() {
   const { data: session } = useSession();
+  console.log(session);
   if (!session) {
     return (
       <div className="bg-blue-900 w-screen h-screen flex items-center">
