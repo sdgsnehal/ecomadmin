aeaef17c0690e010271b90aa25809d2552defff2
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-03-28 10:10:47 +0530

    style(nav.js): improve code formatting for better readability feat(nav.js): add dynamic active link styling based on current pathname feat(next.config.mjs): add configuration for images domain in next.config feat(index.js): add user session data display and image in Home component

--- components/nav.js
+++ components/nav.js
@@ -1,8 +1,11 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Nav = () => {
   const inactiveLink = "flex gap-1 ";
-  const activeLink = inactiveLink +'bg-white text-blue-900 p-1 rounded-l-lg';
+  const activeLink = inactiveLink + "bg-white text-blue-900 p-1 rounded-l-lg";
+  const router = useRouter();
+  const { pathname } = router;
   return (
     <aside className="text-white p-4 pr-0">
       <Link href={"/"} className="flex gap-1 mb-4 mr-2">
@@ -23,7 +26,10 @@ const Nav = () => {
         <span className="">Ecommerce Admin</span>
       </Link>
       <nav className="flex flex-col gap-2">
-        <Link href={"/"} className={activeLink}>
+        <Link
+          href={"/"}
+          className={pathname === "/" ? activeLink : inactiveLink}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -40,7 +46,10 @@ const Nav = () => {
           </svg>
           Dashboard
         </Link>
-        <Link href={"/orders"} className={inactiveLink}>
+        <Link
+          href={"/orders"}
+          className={pathname.includes("/orders") ? activeLink : inactiveLink}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -57,7 +66,10 @@ const Nav = () => {
           </svg>
           Orders
         </Link>
-        <Link href={"/products"} className={inactiveLink}>
+        <Link
+          href={"/products"}
+          className={pathname.includes("/products") ? activeLink : inactiveLink}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -74,7 +86,10 @@ const Nav = () => {
           </svg>
           Products
         </Link>
-        <Link href={"/settings"} className={inactiveLink}>
+        <Link
+          href={"/settings"}
+          className={pathname.includes("/settings") ? activeLink : inactiveLink}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
 import Layout from "@/components/layout";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 
-export default function Home() {
-  return <Layout>TEst</Layout>;
-}
+const Home = () => {
+  const { data: session } = useSession();
+
+  return (
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          Hello,<b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-300 text-black gap-1 overflow-hidden rounded-lg">
+          <Image
+            src={session?.user?.image}
+            alt={session?.user?.name}
+            width={25}
+            height={25}
+            className="rounded-full"
+          />
+          <span className="py-1 px-2">{session?.user?.name}</span>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
