671f2c2ca37cf6d7176216c7e4e395909d4cf7b4
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-03-31 12:54:47 +0530

    feat(api/products.js): add conditional logic to handle GET requests based on query parameters to fetch a single product by id or all products feat(products/edit/[...id].js): add useEffect hook to fetch product data based on id parameter using axios when id changes

--- pages/products/edit/[...id].js
+++ pages/products/edit/[...id].js
@@ -1,8 +1,15 @@
 import Layout from "@/components/layout";
+import axios from "axios";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function EditProductPage() {
   const router = useRouter();
   const { id } = router.query;
+
+  useEffect(() => {
+    if (!id) return;
+    axios.get("/api/products?id=" + id).then((res) => {});
+  }, [id]);
   return <Layout>Edit product form </Layout>;
 }
