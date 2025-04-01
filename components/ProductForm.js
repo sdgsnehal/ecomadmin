fab8a52083eef4bdb5e0f2269a0ce781d801fc60
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-04-01 15:17:30 +0530

    feat(ProductForm): add image upload functionality with file input
    fix(ProductForm): remove unnecessary console log statement
    
    feat(api): add empty upload API endpoint for future image handling

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -7,6 +7,7 @@ const ProductForm = ({
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
+  images,
 }) => {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
@@ -26,7 +27,14 @@ const ProductForm = ({
   if (goToProduct) {
     router.push("/products");
   }
-  console.log(_id);
+  function uploadImages(e) {
+    const files = e.target?.files;
+    if (files.length > 0) {
+      const data = new FormData();
+      files.forEach((file) => data.append("file", file));
+      
+    }
+  }
   return (
     <form onSubmit={saveProduct}>
       <label>Product Name</label>
@@ -36,6 +44,29 @@ const ProductForm = ({
         value={title}
         onChange={(e) => setTitle(e.target.value)}
       />
+      <label>Product Image</label>
+      <div className="mb-2">
+        <label className="w-24 h-24 cursor-pointer text-center flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-300">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-4"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Upload</div>
+          <input type="file" className="hidden" onChange={uploadImages} />
+        </label>
+        {!images?.length && <div>No Photos in this product</div>}
+      </div>
+
       <label>Description</label>
       <textarea
         placeholder="description"
