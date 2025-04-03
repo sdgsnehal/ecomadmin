3889952871633f9d2357cad1fd22ea96a4c40675
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-04-03 11:06:36 +0530

    feat(product-form): improve image handling and support multiple uploads
    feat(api): integrate AWS S3 for file uploads and return uploaded links
    
    feat(deps): add mime-types package for better file type handling

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,5 +1,8 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import multiparty from "multiparty";
-import s3 from "aws-sdk/clients/s3";
+import fs from "fs";
+import mime from "mime-types";
+const bucketName = "your-bucket-name";
 export default async function handler(req, res) {
   const form = new multiparty.Form();
   const { fields, files } = await new Promise((resolve, reject) => {
@@ -8,9 +11,33 @@ export default async function handler(req, res) {
         reject(err);
       }
       resolve({ fields, files });
-      res.json("ok");
     });
   });
+
+  const client = new S3Client({
+    region: "us-east-1",
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
+  const links=[];
+  for (const file of files.file) {
+    const ext = file.originalFilename.split(".").pop();
+    const newFileName = Date.now() + "." + ext;
+    await client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: "newFileName",
+        Body: fs.readFileSync(file.path),
+        ACL: "public-read",
+        ContentType: mime.lookup(file.path),
+      })
+    );
+    const link = "https://${bucketName}.s3.amazonaws.com/${newFileName}";
+    links.push(link);
+  }
+  return res.json(links);
 }
 export const config = {
   api: {
