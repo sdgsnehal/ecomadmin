0ec9250446a8d29643c7ed84f7e49ce65a0dd82d
sdgsnehal <sdgsnehal@gmail.com>
2025-06-08 12:40:19 +0530

    feat(nextauth.js): add support for NEXT_PUBLIC_SECRET environment variable to provide a secret key for authentication style(nextauth.js): fix code formatting by adding semicolons to improve readability and consistency

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,6 +10,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+  secret: process.env.NEXT_PUBLIC_SECRET,
   adapter: MongoDBAdapter(client),
   callbacks: {
     session: async ({ session, token, user }) => {
@@ -26,8 +27,8 @@ export default NextAuth(authOptions);
 export async function isAdminRequest(req, res) {
   const session = await getServerSession(req, res, authOptions);
   if (!adminEmail.includes(session?.user?.email)) {
-    res.status(401)
-    res.end()
+    res.status(401);
+    res.end();
     throw new Error("Not authorized as admin");
   }
 }
