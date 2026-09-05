1774ad47647c823d34e5d7d2800e22e8431012bb
sdgsnehal <sdgsnehal@gmail.com>
2026-09-05 11:59:26 +0530

    delete dialog

--- pages/products/delete/[...id].js
+++ pages/products/delete/[...id].js
@@ -1,10 +1,21 @@
 import Layout from "@/components/layout";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useIsMobile } from "@/hooks/use-mobile";
 import { fetchFromBackend } from "@/lib/fetchfromBackend";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 
 const DeleteProductPage = () => {
   const router = useRouter();
+  const isMobile = useIsMobile();
   const [productInfo, setProductInfo] = useState(null);
   const { id } = router.query;
   useEffect(() => {
@@ -22,19 +33,46 @@ const DeleteProductPage = () => {
     await fetchFromBackend("products/" + id, { method: "DELETE" });
     goBack();
   }
+
+  if (isMobile) {
+    return (
+      <Layout>
+        <h1 className="text-center">
+          Do you really want to delete &nbsp; {productInfo?.name}
+        </h1>
+        <div className="flex gap-2 justify-center">
+          <button className="btn-red" onClick={deleteProduct}>
+            Yes
+          </button>
+          <button className="btn-default" onClick={goBack}>
+            No
+          </button>
+        </div>
+      </Layout>
+    );
+  }
+
   return (
     <Layout>
-      <h1 className="text-center">
-        Do you really want to delete &nbsp; {productInfo?.name}
-      </h1>
-      <div className="flex gap-2 justify-center">
-        <button className="btn-red" onClick={deleteProduct}>
-          Yes
-        </button>
-        <button className="btn-default" onClick={goBack}>
-          No
-        </button>
-      </div>
+      <Dialog open onOpenChange={(open) => !open && goBack()}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Delete product</DialogTitle>
+            <DialogDescription>
+              Do you really want to delete &nbsp;
+              <strong>{productInfo?.name}</strong>?
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button variant="outline" onClick={goBack}>
+              No
+            </Button>
+            <Button variant="destructive" onClick={deleteProduct}>
+              Yes
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </Layout>
   );
 };
